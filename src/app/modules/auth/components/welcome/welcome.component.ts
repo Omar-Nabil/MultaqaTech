f71d52c4d9b40608642d7b1015ad17265d90f0e8
omar <omarabosalem66@gmail.com>
2024-02-23 02:01:44 +0200

    sa

--- src/app/modules/auth/components/welcome/welcome.component.ts
+++ src/app/modules/auth/components/welcome/welcome.component.ts
@@ -12,30 +12,37 @@ import { AuthService } from '../../services/auth.service';
 export class WelcomeComponent implements OnInit {
   signUpForm!:FormGroup;
   logInForm!:FormGroup;
-  error:string = '';
+  logInError:boolean = false;
+  registerError:string = '';
+
   constructor(private _fb:FormBuilder, private _AuthService:AuthService, private _Router:Router) {
 
   }
   ngOnInit(): void {
     main.start();
+    this.createLogInForm();
+    this.createSignUpForm();
 
+  }
+
+  createSignUpForm() {
     this.signUpForm = this._fb.group({
       firstName:['', [Validators.required, Validators.minLength(3)]],
       lastName:['', [Validators.required, Validators.minLength(3)]],
       userName:['', [Validators.required, Validators.minLength(3)]],
       email:['', [Validators.required, Validators.email]],
-      password:['', [Validators.required, Validators.minLength(3), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).*/)]],
+      password:['', [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).*/)]],
       rePassword:['', [Validators.required]],
       phoneNumber:['', [Validators.required, Validators.pattern(/^(?:\+?20|0)?1\d{9}$/)]]
     }, { validators: this.passwordMatchValidator });
-
-    this.logInForm = this._fb.group({
-      userNameOrEmail:['', [Validators.required, Validators.minLength(3)]],
-      password:['', [Validators.required, Validators.minLength(3), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).*/)]],
-    });
   }
 
-
+  createLogInForm() {
+    this.logInForm = this._fb.group({
+      userNameOrEmail:['', [Validators.required, Validators.minLength(3)]],
+      password:['', [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).*/)]],
+    });
+  }
 
   passwordMatchValidator(signUpForm: any){
     let passwordControl = signUpForm.get('password');
@@ -58,28 +65,52 @@ export class WelcomeComponent implements OnInit {
       phoneNumber:  signUp.get('phoneNumber')?.value,
       password:  signUp.get('password')?.value
     };
+    console.log(value);
+
     this._AuthService.register(value).subscribe({
       next:(res) => {
-        console.log(res);
+         console.log(res);
+
+         const modalBackdrops = document.querySelectorAll('.modal-backdrop') as NodeListOf<HTMLElement>;
+          modalBackdrops.forEach(backdrop => {
+            backdrop.classList.add('d-none');
+          });
+
+          this._Router.navigate(['/home']);
+          localStorage.setItem('userToken', res.token);
+          this._AuthService.saveUser();
 
       },
-      error:(err) => console.log(err)
+      error:(err) => {
+        this.registerError = err.error.errors[0];
+        console.log(err);
+
+      }
     })
   }
 
   handleLogIn(logIn:any) {
-    this._AuthService.logIn(logIn).subscribe({
+    const value = {
+      userNameOrEmail: logIn.get('userNameOrEmail')?.value,
+      password:  logIn.get('password')?.value
+    };
+    this._AuthService.logIn(value).subscribe({
       next:(res) => {
-        if(res.message === 'success') {
-          this._Router.navigate(['/home']);
-          localStorage.setItem('userToken', res.token);
-          this._AuthService.saveUser();
-        } else {
-          this.error = res.message;
-        }
+        console.log(res);
 
+        const modalBackdrops = document.querySelectorAll('.modal-backdrop') as NodeListOf<HTMLElement>;
+        modalBackdrops.forEach(backdrop => {
+          backdrop.classList.add('d-none');
+        });
+
+        this._Router.navigate(['/home']);
+        localStorage.setItem('userToken', res.token);
+        this._AuthService.saveUser();
       },
-      error:(err) => console.log(err)
+      error:(err) => {
+        this.logInError = true;
+        console.log(err);
+      }
 
     })
   }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,15 +10,16 @@ export class AuthService {
   constructor(private _HttpClient:HttpClient) { }
   userData:BehaviorSubject<any> = new BehaviorSubject<any>(null);
   register(value:any):Observable<any> {
-    return this._HttpClient.post(environment.baseURL+'/api/Account/register', value);
+    return this._HttpClient.post('/api/Account/register', value);
   }
   logIn(value:any):Observable<any> {
-    return this._HttpClient.post(environment.baseURL+'/api/Account/login', value);
+    return this._HttpClient.post('/api/Account/login', value);
   }
 
   saveUser():void {
     let token = JSON.stringify(localStorage.getItem('userToken'));
     let decode:any = jwtDecode(token);
     this.userData.next(decode);
+
   }
 }
