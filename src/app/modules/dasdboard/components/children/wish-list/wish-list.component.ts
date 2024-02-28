c483811d453b4c66f846167b4269989b55c3f155
omar <omarabosalem66@gmail.com>
2024-02-28 07:33:45 +0200

    display wish-list

--- src/app/modules/dasdboard/components/children/wish-list/wish-list.component.ts
+++ src/app/modules/dasdboard/components/children/wish-list/wish-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 @Component({
   selector: 'app-wish-list',
   templateUrl: './wish-list.component.html',
   styleUrls: ['./wish-list.component.scss']
 })
-export class WishListComponent {
-
+export class WishListComponent implements OnInit {
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    Aos.init({
+      once: true,
+      offset: 0,
+    });
+  }
 }
