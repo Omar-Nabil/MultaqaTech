4197add6a24a48dc25d0b5dd7a141357b33ed84b
Ahmed Talat <ahmedtalaat20011@gmail.com>
2024-03-12 15:05:25 +0200

    add course form

--- src/app/modules/courses/interfaces/course.ts
+++ src/app/modules/courses/interfaces/course.ts
@@ -1,10 +1,10 @@
 export interface Course_add {
-    subjectId: number,
+  subjectId: number,
   title: string,
   language: string,
   thumbnailUrl: string,
-  price: string,
-  courseLevel: string,
+  price: number,
+  courseLevel: number,
   tagsIds: number[],
   prerequisitesIds: number[],
   learningObjectives: string[]
--- src/app/modules/dasdboard/components/main/main.component.ts
+++ src/app/modules/dasdboard/components/main/main.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup,  Validators} from '@angular/forms';
 import { SubjectService } from '../../services/subject.service';
 import { Subject } from '../../interfaces/subject';
+import { Course_add } from 'src/app/modules/courses/interfaces/course';
+import { title } from 'process';
 
 @Component({
   selector: 'app-main',
@@ -10,7 +12,21 @@ import { Subject } from '../../interfaces/subject';
 })
 export class MainComponent {
 
-  subjects:Subject[]=[]
+  subjects: Subject[] = []
+  tags:number[]=[]
+  prerequisites:number[]=[]
+  l_0bjects:string[]=[]
+    course:Course_add = {
+    subjectId: 0,
+  title: 'string',
+  language: 'string',
+  thumbnailUrl: 'string',
+  price: 0,
+  courseLevel: 0,
+  tagsIds: [],
+  prerequisitesIds: [],
+  learningObjectives: []
+  }
 
   constructor(private _SubjectService:SubjectService){}
 
@@ -22,8 +38,13 @@ export class MainComponent {
   addCourseForm:FormGroup = new FormGroup({
     title:new FormControl(''),
     language:new FormControl(''),
+    img:new FormControl(''),
+    price:new FormControl(''),
+    learningobjectives:new FormControl(''),
     subject:new FormControl(0),
-    courselevel:new FormControl('ee'),
+    tags:new FormControl(0),
+    prerequisites:new FormControl(0),
+    courselevel:new FormControl('0'),
   })
 
     getSubjects() {
@@ -32,5 +53,27 @@ export class MainComponent {
          this.subjects = response;
       }
     })
+    }
+
+
+  log() {
+    this.course.title=this.addCourseForm.get('title')?.value
+    this.course.language =this.addCourseForm.get('language')?.value
+    this.course.thumbnailUrl =this.addCourseForm.get('img')?.value
+    this.course.price =this.addCourseForm.get('price')?.value
+    this.course.subjectId = parseInt(this.addCourseForm.get('subject')?.value)
+    this.tags.push(parseInt(this.addCourseForm.get('tags')?.value))
+    this.course.tagsIds = this.tags
+    this.prerequisites.push(parseInt(this.addCourseForm.get('prerequisites')?.value))
+    this.course.prerequisitesIds=this.prerequisites
+    this.course.courseLevel = parseInt(this.addCourseForm.get('courselevel')?.value)
+    this.l_0bjects.push(this.addCourseForm.get('learningobjectives')?.value)
+    console.log(this.course);
+
+  }
+
+  clicked(name:string) {
+    console.log(name);
+
   }
 }
