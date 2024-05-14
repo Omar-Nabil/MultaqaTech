45e557882c1df686c560601c98c0372f40e10fbd
AhmedTarek10 <ahmedtarekessa10@gmail.com>
2024-05-15 02:07:39 +0300

    adding meeting form fixed

--- src/app/modules/zoom/components/zoom-meetings/zoom-meetings.component.ts
+++ src/app/modules/zoom/components/zoom-meetings/zoom-meetings.component.ts
@@ -13,6 +13,8 @@ export class ZoomMeetingsComponent implements OnInit {
   meetings:any[] = [];
   categories:any[] = [];
   addMeetingForm!: FormGroup;
+  previewUrl: string | ArrayBuffer | null = null;
+  selectedFile: File | null = null;
   constructor(private _ZoomsService:ZoomsService){
 
   }
@@ -37,18 +39,55 @@ export class ZoomMeetingsComponent implements OnInit {
 
 
   }
-  addMeeting() {
-    const meetingData = {
-      Title: this.addMeetingForm.get('title')?.value,
-      Content: this.addMeetingForm.get('content')?.value,
-      startDate: this.addMeetingForm.get('startDate')?.value,
-      categoryId: this.addMeetingForm.get('categoryId')?.value,
-      duration: this.addMeetingForm.get('duration')?.value,
-      PictureUrl: this.addMeetingForm.get('pictureUrl')?.value,
-      TimeZone: this.addMeetingForm.get('timeZone')?.value,
-    }
+  // addMeeting() {
+  //   const meetingData = {
+  //     Title: this.addMeetingForm.get('title')?.value,
+  //     Content: this.addMeetingForm.get('content')?.value,
+  //     startDate: this.addMeetingForm.get('startDate')?.value,
+  //     categoryId: this.addMeetingForm.get('categoryId')?.value,
+  //     duration: this.addMeetingForm.get('duration')?.value,
+  //     PictureUrl: this.addMeetingForm.get('pictureUrl')?.value,
+  //     TimeZone: this.addMeetingForm.get('timeZone')?.value,
+  //   }
 
-    this._ZoomsService.postMeeting(meetingData).subscribe({
+  //   this._ZoomsService.postMeeting(meetingData).subscribe({
+  //     next:(res) => {
+  //       console.log(res);
+  //       this.meetings.unshift(res);
+  //     },
+  //     error:(err) => console.log(err)
+
+  //   })
+  // }
+  onFileChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if (file) {
+      this.selectedFile = file;
+
+      const reader = new FileReader();
+      reader.onload = () => this.previewUrl = reader.result;
+      reader.readAsDataURL(file);
+    }
+  }
+  addMeeting() {
+    const formData = new FormData();
+    formData.append('title', this.addMeetingForm.get('title')?.value);
+    formData.append('content', this.addMeetingForm.get('content')?.value);
+    formData.append('startDate', this.addMeetingForm.get('startDate')?.value);
+    formData.append('TimeZone', this.addMeetingForm.get('TimeZone')?.value);
+    formData.append('startDate', this.addMeetingForm.get('startDate')?.value);
+    formData.append('duration', this.addMeetingForm.get('duration')?.value);
+    formData.append('categoryId', this.addMeetingForm.get('categoryId')?.value);
+
+    // Append the file to formData
+    if (this.selectedFile) {
+      formData.append('pictureUrl', this.selectedFile, this.selectedFile.name);
+    }
+    console.log(formData);
+
+    this._ZoomsService.postMeeting(formData).subscribe({
       next:(res) => {
         console.log(res);
         this.meetings.unshift(res);
